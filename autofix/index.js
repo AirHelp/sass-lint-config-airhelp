baaf8e3fe89f1bc094581a25ca531b63567c5991
Marcin Horoszko <marcin.horoszko@airhelp.com>
2017-02-01 15:01:52 +0100

    prepared repository for running autofix utility

--- autofix/index.js
+++ autofix/index.js
@@ -1,16 +1,18 @@
 const gonzales = require('gonzales-pe');
 const fs = require('fs');
 const path = require('path');
-const rules = require('./rules');
 const walkSync = require('walk-sync');
+const yargs = require('yargs');
+const rules = require('./rules');
 
+const { argv } = yargs;
+const { log } = console;
+
+// configuration
 const root = path.join(path.dirname(require.main.filename), '..');
 
-const fixDirectories = [
-  'app/components',
-  'app/styles',
-  '../app/assets/stylesheets',
-];
+// prepare lookup
+const fixDirectories = argv._;
 
 const scssFiles = fixDirectories.reduce((currentFiles, directory) => {
   const directoryPath = path.join(root, directory);
@@ -23,13 +25,13 @@ const scssFiles = fixDirectories.reduce((currentFiles, directory) => {
   return currentFiles;
 }, []);
 
-// create statistics rule map
+// define statistics rule map
 const fixStatistics = {};
 Object.keys(rules).forEach(rule => {
   fixStatistics[rule] = 0;
 });
 
-// for each file
+// process fixing for each file
 scssFiles.forEach(file => {
   const fullPath = path.join(root, file);
   const contents = fs.readFileSync(fullPath, { encoding: 'utf8' });
@@ -54,6 +56,7 @@ scssFiles.forEach(file => {
   });
 });
 
+// display fix statistics
 Object.keys(fixStatistics).forEach(rule => {
   console.log(`Fixed ${rule} warnings: ${fixStatistics[rule]}`);
 });
